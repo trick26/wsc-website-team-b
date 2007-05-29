ddc794d771c1fa04ff26ce56886abc8be182f165
birderrob <birderrob@8b9f0045-c12f-0410-8833-7f1c27021469>
2007-05-29 03:48:35 +0000

    ongoing development

--- website/App_Code/BLL/Client.cs
+++ website/App_Code/BLL/Client.cs
@@ -13,10 +13,65 @@ using System.Web.UI.HtmlControls;
 /// </summary>
 public class Client
 {
-	public Client()
+    DataSet dataSet = new DataSet();
+    DataSet.clientsRow theClient;
+    
+    public Client()
 	{
 		//
 		// TODO: Add constructor logic here
 		//
 	}
+
+    public int ClientId
+    {
+        get { return theClient.clientId; }
+    }
+
+    public void LoadClientById(int clientId)
+    {
+        theClient = dataSet.clients.FindByclientId(clientId);
+    }
+
+    public String ClientName
+    {
+        get { return theClient.clientName; }
+        set { theClient.clientName = value; }
+    }
+
+    public String ClientAddress
+    {
+        get { return theClient.clientAddress; }
+        set { theClient.clientAddress = value; }
+    }
+	
+    public String ClientCity
+    {
+        get { return theClient.clientCity; }
+        set { theClient.clientCity = value; }
+    }
+
+    public String ClientState
+    {
+        get { return theClient.clientState; }
+        set { theClient.clientState = value; }
+    }
+
+    public String ClientZip
+    {
+        get { return theClient.clientZip; }
+        set { theClient.clientZip = value; }
+    }
+
+    public String ClientPhone
+    {
+        get { return theClient.clientPhone; }
+        set { theClient.clientPhone = value; }
+    }
+
+    public String ClientContactName
+    {
+        get { return theClient.clientContactName; }
+        set { theClient.clientContactName = value; }
+    }
 }
