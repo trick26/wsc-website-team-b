d04851545ff09d42ddd79279f6daa9190c232c31
birderrob <birderrob@8b9f0045-c12f-0410-8833-7f1c27021469>
2007-06-06 21:45:24 +0000

    ongoing development

---
+++ trunk/website/App_Code/BLL/Cart.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+/// <summary>
+/// Summary description for Cart
+/// </summary>
+public class Cart
+{
+	public Cart()
+	{
+	}
+
+    public Cart(String userName, String details, int catalogId)
+    {
+        this.UserName = userName;
+        this.Details = details;
+        this.CatalogId = catalogId;
+    }
+
+    private String _userName;
+
+    public String UserName
+    {
+        get { return _userName; }
+        set { _userName = value; }
+    }
+
+    private String _details;
+
+    public String Details
+    {
+        get { return _details; }
+        set { _details = value; }
+    }
+
+    private int _catalogId;
+
+    public int CatalogId
+    {
+        get { return _catalogId; }
+        set { _catalogId = value; }
+    }
+}
